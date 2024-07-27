b277b148780feb0200e761650106b535e52f0a2c
sangam5756 <sangammunde3@gmail.com>
2024-07-28 00:33:55 +0530

    cross-access-cors-solved

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,22 @@ import path from "path";
 const PORT = process.env.PORT || 5001;
 const app = express();
 
+const allowedOrigins = ['http://localhost:5173', 'https://petfriend5756.onrender.com'];
+
 
 app.use(express.json());
-app.use(
-  cors({
-    // origin: "http://localhost:5173", // Your frontend URL
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Your frontend URL
-    // optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl requests, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.use(cookieParser());
 
